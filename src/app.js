603c0d5b82d8e0b4c1a2b32dc2bf20c384b5478a
Eyetsko <104879063+Eyetsko@users.noreply.github.com>
2022-07-22 16:20:31 -0400

    added current date/time function and api info for weather location by city
    api call not functioning

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
-let apiKey = "210f0105830dab42da6932b5c4b52fc7";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
-console.log(apiUrl);
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+let updatedTime = document.querySelector(`#dateTime`);
+let current = new Date();
+updatedTime.innerHTML = formatDate(current);
+
+function showWeather(response) {
+  document.querySelector(`#city-name`).innerHTML = response.data.name;
+}
+function search(city) {
+  let apiKey = "210f0105830dab42da6932b5c4b52fc7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+function citySearch(event) {
+  event.preventDefault();
+  let city = document.querySelector(`#search`).value;
+  search(city);
+}
