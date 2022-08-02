27a5dd8a129b2ab81afdda0e7287035ddea08e0f
Eyetsko <104879063+Eyetsko@users.noreply.github.com>
2022-08-02 16:35:26 -0400

    updated JS
    form now functioning using API search by city
    icon now working using API search by city
    Current date/time now working for my location

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -9,16 +18,9 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${day} ${hours}:${minutes}`;
+  return ` ${day} ${hours}:${minutes}`;
 }
+
 function displayTemperature(response) {
   console.log(response.data);
   let temp = document.querySelector("#temp");
@@ -30,6 +32,8 @@ function displayTemperature(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = Math.round(response.data.main.humidity);
   let description = document.querySelector("#description");
+  let date = document.querySelector("#date-time");
+  date.innerHTML = formatDate(response.data.dt * 1000);
   description.innerHTML = response.data.weather[0].description;
   let icon = document.querySelector("#icon");
   icon.setAttribute(
@@ -38,8 +42,17 @@ function displayTemperature(response) {
   );
   icon.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "1b8da53efbc507799e8037efe6fb300e";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "1b8da53efbc507799e8037efe6fb300e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function submit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+search("New York");
+
+let form = document.querySelector("#search");
+form.addEventListener("submit", submit);
