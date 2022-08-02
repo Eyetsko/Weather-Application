21a2f804a5753bd0ddfeaff0b65fa20189b58f2a
Eyetsko <104879063+Eyetsko@users.noreply.github.com>
2022-08-02 17:25:08 -0400

    Updated C/F links
    Changed C and F to links, made other active when one is clicked.
    Added function to convert celsius to Farenheit, and reverse when clicking C/F

--- src/app.js
+++ src/app.js
@@ -23,8 +23,9 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   console.log(response.data);
+  celsiusTemp = response.data.main.temp;
   let temp = document.querySelector("#temp");
-  temp.innerHTML = Math.round(response.data.main.temp);
+  temp.innerHTML = Math.round(celsiusTemp);
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
   let wind = document.querySelector("#wind");
@@ -34,6 +35,7 @@ function displayTemperature(response) {
   let description = document.querySelector("#description");
   let date = document.querySelector("#date-time");
   date.innerHTML = formatDate(response.data.dt * 1000);
+
   description.innerHTML = response.data.weather[0].description;
   let icon = document.querySelector("#icon");
   icon.setAttribute(
@@ -44,7 +46,7 @@ function displayTemperature(response) {
 }
 function search(city) {
   let apiKey = "1b8da53efbc507799e8037efe6fb300e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 function submit(event) {
@@ -52,7 +54,32 @@ function submit(event) {
   let cityInput = document.querySelector("#city-input");
   search(cityInput.value);
 }
-search("New York");
+function showFarenheit(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  farenheitLink.classList.add("active");
+  let farenheit = (celsiusTemp * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(farenheit);
+}
+function showCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  farenheitLink.classList.remove("active");
+  let celsius = celsiusTemp;
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsius);
+}
+
+let celsiusTemp = null;
 
 let form = document.querySelector("#search");
 form.addEventListener("submit", submit);
+
+let farenheitLink = document.querySelector("#farenheit");
+farenheitLink.addEventListener("click", showFarenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsius);
+
+search("New York");
